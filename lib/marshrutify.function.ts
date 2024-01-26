b45a68a858ad2c209b1e866de7b6d7ff34e619d4
Aliaksei Pialetski <Aliaksei_Pialetski@epam.com>
2024-01-26 18:22:59 +0300

    fix: lambda HandlerNotFound

--- lib/marshrutify.function.ts
+++ lib/marshrutify.function.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export const lambdaHandler = async (
+export const handler = async (
   _: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   return Promise.resolve<APIGatewayProxyResult>({
